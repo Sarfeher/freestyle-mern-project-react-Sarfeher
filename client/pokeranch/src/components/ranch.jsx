825bd0009c59f006258468ab4463a758dbcb379d
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2023-10-02 16:02:10 +0200

    add ranch component, ranch api

---
+++ client/pokeranch/src/components/ranch.jsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from "react";
+
+
+
+
+function Ranch(){
+    const [pokemon, setPokemon] = useState([]);
+
+    useEffect(()=>{
+        fetch("api/ranch")
+    }[pokemon])
+
+
+
+
+
+
+return (<div>
+
+</div>)
+}
+
+
+export default Ranch;
--- server/server.js
+++ server/server.js
@@ -8,23 +8,14 @@ app.use(express.json())
 mongoose.connect("mongodb+srv://sarfeher:Toyotacorolla20201.8@sarfeher.hft4jys.mongodb.net/pokemon")
 
 
-//Create pokemon from the server
-/* Pokemon.create({
-    name: 'psyduck',
-    nickName: 'beep',
-    front: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/54.gif ",
-    back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/54.gif",
-    hp: 35,
-    attack: 35,
-    defense: 35,
-    xp: 0,
+app.get("api/ranch", (res,req)=>{
+    const pokemon = Pokemon.find()
+    console.log(pokemon)
 })
-.then(pokemon => {
-    console.log(pokemon);
-})
-.catch(error => {
-    console.error(error)
-}); */
+
+
+
+
 
 app.listen(3000, () => {
     console.log('Im in! Open this link: http://127.0.0.1:3000');
